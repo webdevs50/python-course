dff68fa18e34a76233767af161cc98af7dda0540
webdevs50 <seniorsdevs@gmail.com>
2025-03-25 17:13:45 -0400

    Lesson 1 finished

---
+++ LESSON_1/app.py
@@ -0,0 +1,39 @@
+## Lession 1 is about variables in Python
+
+print("Hello, Khaled"),
+print('o----');
+print(' |||| ');
+print('&' * 10);
+
+################  Variables #######################
+
+#### Assigining Integers to varlables
+price = 10
+print(price)
+
+price = 20
+print(price)
+
+#### Assigining Floats to varlables
+rating = 4.9
+print(rating)
+
+
+#### Assigining Strings to varlables
+firstName = 'Khaled'
+lastName = 'Ali'
+
+print(firstName)
+print(lastName)
+
+#### Assigining Booleans to varlables
+is_published = True
+print(is_published)
+
+
+### Test
+patient_name = 'John Smith'
+age = 20
+is_new = True
+
+print('We check in a patient named ' + patient_name + '. He\'s' + age)
